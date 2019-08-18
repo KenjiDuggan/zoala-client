dea9decb31f62b6555479270734aea3e47d02737
KenjiDuggan <kenjiduggan@gmail.com>
2019-08-17 23:28:16 -0400

    nice

--- pages/muscles/_muscle_id/edit.vue
+++ pages/muscles/_muscle_id/edit.vue
@@ -33,13 +33,11 @@
           </template>
           <v-card>
             <v-card-text>
-              <v-form @submit="editnow">
-                <v-textarea v-model="bodypartedit" rows="1" :label="muscleid.schedule[i].bodyPart" />
-                <v-textarea v-for="(jtem, j) in muscleid.schedule[i].workouts.length" :key="j" v-model="workoutseditPlaceholders[j]" rows="1" :label="muscleid.schedule[i].workouts[j]" />
-                <v-btn @click="submit">
-                  Edit Day
-                </v-btn>
-              </v-form>
+              <v-textarea v-model="bodypartedit" rows="1" :label="muscleid.schedule[i].bodyPart" />
+              <v-textarea v-for="(jtem, j) in muscleid.schedule[i].workouts.length" :key="j" v-model="workoutseditPlaceholders[j]" rows="1" :label="muscleid.schedule[i].workouts[j]" />
+              <v-btn @click="editday(i)">
+                Edit Day
+              </v-btn>
             </v-card-text>
           </v-card>
         </v-expansion-panel-content>
@@ -63,44 +61,44 @@ export default {
       ktems: 4,
       length: 3,
       bodypartedit: '',
-      workoutseditPlaceholders: ['', '', '', '', '', '', '', '', '', '', '', ''],
-      editworkouts: [
-        {
-          day: this.$store.state.muscleid.schedule[0].day,
-          bodypart: this.$store.state.muscleid.schedule[0].bodyPart,
-          workouts: this.$store.state.muscleid.schedule[0].workouts
-        },
-        {
-          day: this.$store.state.muscleid.schedule[1].day,
-          bodypart: this.$store.state.muscleid.schedule[1].bodyPart,
-          workouts: this.$store.state.muscleid.schedule[1].workouts
-        },
-        {
-          day: this.$store.state.muscleid.schedule[2].day,
-          bodypart: this.$store.state.muscleid.schedule[2].bodyPart,
-          workouts: this.$store.state.muscleid.schedule[2].workouts
-        },
-        {
-          day: this.$store.state.muscleid.schedule[3].day,
-          bodypart: this.$store.state.muscleid.schedule[3].bodyPart,
-          workouts: this.$store.state.muscleid.schedule[3].workouts
-        },
-        {
-          day: this.$store.state.muscleid.schedule[4].day,
-          bodypart: this.$store.state.muscleid.schedule[4].bodyPart,
-          workouts: this.$store.state.muscleid.schedule[4].workouts
-        },
-        {
-          day: this.$store.state.muscleid.schedule[5].day,
-          bodypart: this.$store.state.muscleid.schedule[5].bodyPart,
-          workouts: this.$store.state.muscleid.schedule[5].workouts
-        },
-        {
-          day: this.$store.state.muscleid.schedule[6].day,
-          bodypart: this.$store.state.muscleid.schedule[6].bodyPart,
-          workouts: this.$store.state.muscleid.schedule[6].workouts
-        }
-      ]
+      workoutseditPlaceholders: ['', '', '', '', '', '', '', '', '', '', '', '']
+      // editworkouts: [
+      //   {
+      //     day: this.$store.state.muscleid.schedule[0].day,
+      //     bodypart: this.$store.state.muscleid.schedule[0].bodyPart,
+      //     workouts: this.$store.state.muscleid.schedule[0].workouts
+      //   },
+      //   {
+      //     day: this.$store.state.muscleid.schedule[1].day,
+      //     bodypart: this.$store.state.muscleid.schedule[1].bodyPart,
+      //     workouts: this.$store.state.muscleid.schedule[1].workouts
+      //   },
+      //   {
+      //     day: this.$store.state.muscleid.schedule[2].day,
+      //     bodypart: this.$store.state.muscleid.schedule[2].bodyPart,
+      //     workouts: this.$store.state.muscleid.schedule[2].workouts
+      //   },
+      //   {
+      //     day: this.$store.state.muscleid.schedule[3].day,
+      //     bodypart: this.$store.state.muscleid.schedule[3].bodyPart,
+      //     workouts: this.$store.state.muscleid.schedule[3].workouts
+      //   },
+      //   {
+      //     day: this.$store.state.muscleid.schedule[4].day,
+      //     bodypart: this.$store.state.muscleid.schedule[4].bodyPart,
+      //     workouts: this.$store.state.muscleid.schedule[4].workouts
+      //   },
+      //   {
+      //     day: this.$store.state.muscleid.schedule[5].day,
+      //     bodypart: this.$store.state.muscleid.schedule[5].bodyPart,
+      //     workouts: this.$store.state.muscleid.schedule[5].workouts
+      //   },
+      //   {
+      //     day: this.$store.state.muscleid.schedule[6].day,
+      //     bodypart: this.$store.state.muscleid.schedule[6].bodyPart,
+      //     workouts: this.$store.state.muscleid.schedule[6].workouts
+      //   }
+      // ]
     }
   },
   computed: {
@@ -112,8 +110,13 @@ export default {
     all() {
       this.panel = [...Array(this.items).keys()].map((k, i) => i)
     },
-    editday(k) {
+    editday(i) {
+      console.log(this.muscleid.schedule[i].bodyPart) // eslint-disable-line
+      this.muscleid.schedule[i].bodyPart = this.bodypartedit
 
+      console.log(this.$store.state.muscleid.schedule[i].bodyPart) // eslint-disable-line
+      console.log(this.bodypartedit)  // eslint-disable-line
+      this.$store.state.muscleid.schedule[i].workouts = this.workoutseditPlaceholders
     },
     editweek(k) {
 
